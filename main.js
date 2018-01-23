651a445907d1607878a8547846eee603dd8cfd10
Katy <ihatetoast@gmail.com>
2018-01-23 08:14:12 -0600

    starting over working inside out

--- main.js
+++ main.js
@@ -20,55 +20,41 @@
 
 const inquirer = require('inquirer');
 
-const Word = require('./word');
-const Letter = require('./letter');
-const GameWord = require('./gameword');
-
 //do you want to play a game
 'use strict';
 function playGame(){
   inquirer
-  .prompt([
-    {
-      type: "confirm",
+  .prompt(
+    [{
+      name: "player",
+      message: "What is your name?",
+    },
+      {
+      type: "list",
       name: "playgame",
       message: "Do you want to play hangman?",
-      default: true
+      choices: ["Nah. I'd rather chew glass.", "Not now. Ask me later.", "Do I ever! I'm the best at this game. Prepared to be smoked"]
     }] 
   )
-  .then(function(answer){
-    if(!answer.playgame){
-      console.log("Bugger. Come back soon.");
-      setTimeout(()=>{
-        console.log("😁 Ok. What about now?");
-        playGame();
-      }, 3000);
+  .then (function (answer){
+    if(answer.playgame === choices[0]){
+      console.log(`I get it, ${answer.player}. I'm not into old west justice, either.`);
     }
-    else {
-      console.log("player wants to play");
-      const wordArr = ['Game', 'Animals', 'Plants', 'Emotion', 'Programming','Texas', 'Australia', 'Sports', 'Art', 'Literature', 'Food'];
-      //get the theme
-      inquirer
-      .prompt(
-        {
-          type: 'list',
-          name: 'theme',
-          message: 'What theme do you want?',
-          choices: wordArr,
-          filter: function(val) {
-            return val.toLowerCase();
-          }
-        }
-      )
-      .then(function(answer){
-        const theme = answer.theme;
-        //pass theme into the word fcn
-        console.log(`user's chosen theme is ${theme}`);
-      })
-      
+    else if(answer.playGame === choices[1]){
+      console.log(`I understand, ${answer.player}.`);
+      setTimeout(()=>{
+        console.log("How about now?");
+        playGame();
+      }, 2000);
+    }
+    else{
+      console.log(`Ah, you're the best, ${answer.player}.`);
     }
   })
-};
+  }
+  .then(function(answer){
+    
+}; //end fcn playGame
 
 playGame();
 
